7cbb74f8637cf688b327ef94d291ed17a581a9dd
Daniel Moraes <daniel.b.moraes@gmail.com>
2016-06-10 10:16:52 -0300

    Added code folder.

--- 1/1.2-2.py
+++
@@ -1,5 +0,0 @@
-import math
-
-for i in xrange(1, 100):
-    print "%d, %10.2f, %10.2f\n" % (i, 8 * math.pow(i, 2),
-                                    64 * i * math.log(i, 2),)
--- 1/1.2-3.py
+++
@@ -1,4 +0,0 @@
-import math
-
-for i in xrange(1, 100):
-    print "%d, %10.2f, %10.2f\n" % (i, 100 * math.pow(i, 2), math.pow(2, i),)
